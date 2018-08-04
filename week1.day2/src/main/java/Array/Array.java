4635d0f6437dca5df6f92d1329d31910817fd114
champak <champak@DESKTOP-JNJUSHR>
2018-08-04 18:10:53 +0530

    Array value changed

--- week1.day2/src/main/java/Array/Array.java
+++ week1.day2/src/main/java/Array/Array.java
@@ -19,7 +19,8 @@ public class Array {
 		System.out.println(" Grade in 2nd subject is: " + grade[index][1]);
 		System.out.println(" Grade in 3rd subject is: " + grade[index][2]);
 		System.out.println(" Grade in 4th subject is: " + grade[index][3]);
-		System.out.println(" Grade in 5th subject is: " + grade[index][4]);
+		//System.out.println(" Grade in 5th subject is: " + grade[index][4]);
+		Champak:
 	}
 	}
 
